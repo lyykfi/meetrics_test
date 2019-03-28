fe771929353ade8602246ddc7953123c825c7518
Andrey Makar-Uvarov <andrey.makar-uvarov@hugo.events>
2019-03-28 20:56:18 +0100

    first commit

--- src/script.js
+++ src/script.js
@@ -67,24 +67,29 @@ class AdvManager {
     if (this.pageIsVisible) {
       const scrollTop = document.documentElement.scrollTop;
       const elementYPosition = rec.top + scrollTop;
-      if ((rec.top > 0 && scrollTop + window.innerHeight >= elementYPosition) ||
-          (rec.top < 0 && Math.abs(rec.top) < rec.height)) {
+      if ((rec.top > 0 &&
+           scrollTop + window.innerHeight - rec.height >= elementYPosition) &&
+           rec.left > 0) {
         this.viewabilityTime += 1;
         this.isViewable = true;
-        if (rec.top < 0) {
-          this.percentOfVisibility = ((Math.abs(rec.height + rec.top) / rec.height) * 100).toFixed(1);
-        } else {
-          if (rec.height + rec.top > window.innerHeight) {
-            const gap = (elementYPosition + rec.height) - (scrollTop + window.innerHeight);
-            this.percentOfVisibility = (100 - (gap / rec.height) * 100).toFixed(1);
-          } else {
-            this.percentOfVisibility = 100;
-          }
-        }
-        
       } else {
         this.isViewable = false;
-        this.percentOfVisibility = 0;
+
+        if ((rec.top > 0 && scrollTop + window.innerHeight >= elementYPosition) ||
+          (rec.top < 0 && Math.abs(rec.top) < rec.height)) {
+          if (rec.top < 0) {
+            this.percentOfVisibility = ((Math.abs(rec.height + rec.top) / rec.height) * 100).toFixed(1);
+          } else {
+            if (rec.height + rec.top > window.innerHeight) {
+              const gap = (elementYPosition + rec.height) - (scrollTop + window.innerHeight);
+              this.percentOfVisibility = (100 - (gap / rec.height) * 100).toFixed(1);
+            } else {
+              this.percentOfVisibility = 100;
+            }
+          }
+        } else {
+          this.percentOfVisibility = 0;
+        }
       }
     } else {
       this.isViewable = false;
